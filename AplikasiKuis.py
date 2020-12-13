7893eddbd0f3828d6af756d02d042b626e2c2923
mareta103 <74005509+mareta103@users.noreply.github.com>
2020-12-13 19:54:35 +0700

    Update AplikasiKuis.py

--- AplikasiKuis.py
+++ AplikasiKuis.py
@@ -2,4 +2,46 @@
 # GAME KUIS SEDERHANA PERKALIAN MATEMATIKA
 
 print("Game Matermatika")
-
+def soal_pertama():
+    X = int(input(" 4 * 5 = "))
+    if (X == 20):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_kedua():
+    Y = int(input(" 2 * 3 = "))
+    if (Y == 6):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_ketiga():
+    Z = int(input(" 8 * 9 = "))
+    if (Z == 72):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_keempat():
+    A = int(input(" 3 * 9 = "))
+    if (A == 27):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_kelima():
+    B = int(input(" 15 * 4 = "))
+    if (B == 60):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_keenam():
+    C = int(input(" 6 * 8 = "))
+    if (C == 48):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_ketujuh():
+    D = int(inpuy(" 9 * 12 = "))
+    if (D == 108):
+        print("Benar!! ")
+    else:
+        print*"Yah salah")
+      
