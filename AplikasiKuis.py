4115cbebe58a0fcb4340816b3b901cd3a8df43e1
dani1006 <73922058+dani1006@users.noreply.github.com>
2020-12-13 20:21:17 +0700

    Update AplikasiKuis.py

--- AplikasiKuis.py
+++ AplikasiKuis.py
@@ -43,5 +43,47 @@ def soal_ketujuh():
     if (D == 108):
         print("Benar!! ")
     else:
-        print*"Yah salah")
+        print("Yah salah")
+def soal_kedelapan():
+    E = int(input(" 3 x 1 = ")
+    if (E == 3):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_kesembilan():
+    F= int(input(" 4 x 24 = "))
+    if (F== 96):
+        print("Benar!! ")
+    else:
+        print("Yah salah")
+def soal_kesepuluh():
+    G= int(input(" 7 x 9 = "))
+    if (G== 54):
+        print("Benar!! ")
+    else:
+        print("Yah salah ")   
+            
+def show_menu():
+    print()
+    print("----Istirahat duluu----")
+    print()
+    print("Lanjut? ")
+    print("1. Ya ")
+    print("2. Tidak ")
+    
+    menu = int(input(" Pilih manu: "))
+    print()
+    
+    if menu == 1:
+        soal_keenam()
+        soal_ketujuh()
+        soal_kedelapan()
+        soal_kesembilan()
+        soal_kesepuluh()
+    elif menu == 2:
+        exit()
+    else:
+        print("wah ada yang salah!")
+        
+soal_pertama()
       
